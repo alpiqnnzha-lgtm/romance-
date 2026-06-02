1c43fa7edc4132495663b487022c5d82f5922eed
alpiqnnzha-lgtm <alpiqnnzha@gmail.com>
2026-06-02 23:34:14 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const pages = document.querySelectorAll('.page');
 let currentPage = 0;
 
-document.addEventListener('click', function(e) {
-    if (e.target.id === 'btnYes') return;
+function nextPage() {
+    pages[currentPage].classList.remove('active');
+    currentPage++;
+    pages[currentPage].classList.add('active');
+    if(currentPage === 1) typeText();
+}
 
-    if (currentPage < pages.length - 1) {
-        pages[currentPage].classList.remove('active');
-        currentPage++;
-        pages[currentPage].classList.add('active');
+const text = "Hai sayang... Terima kasih ya sudah hadir di hidupku. Setiap hari bersamamu adalah anugerah terindah yang pernah aku miliki. Aku berharap kita bisa terus bersama melewati suka dan duka selamanya. Kamu adalah orang yang paling aku sayang.";
+
+function typeText() {
+    let i = 0;
+    const element = document.getElementById("typing-text");
+    element.innerHTML = "";
+    function typing() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typing, 50);
+        }
     }
-});
+    typing();
+}
